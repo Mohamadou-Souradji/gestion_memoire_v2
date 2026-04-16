9609535bfe12b95346c22d6cf75571181abfa538
Souradji <souradjimohamadou@gmail.com>
2026-04-16 18:54:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-# SUPPRIME la ligne makemigrations (on ne le fait qu'en local)
 python manage.py migrate --no-input
-
-# À laisser uniquement si tu veux réinitialiser les données ESCEP à chaque build
-# python manage.py charger_donnees_test
