d6d231954dc9da1825908ba0a48651fcfcf442e6
Souradji <souradjimohamadou@gmail.com>
2026-04-16 17:58:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Installation et statiques
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations --no-input
+
+# 1. On force le marquage de la migration problématique comme "faite"
+python manage.py migrate etudiant 0003 --fake
+
+# 2. On lance toutes les autres migrations normalement
 python manage.py migrate --no-input
+
+# 3. On charge les données ESCEP
 python manage.py charger_donnees_test
